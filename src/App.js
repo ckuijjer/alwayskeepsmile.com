5c4050fa010cea0b0b2068cbb0b12e24c12132c1
Casper Kuijjer <casper@kuijjer.com>
2018-03-24 14:06:37 +0100

    🗑️

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react'
 import { WindowSize } from 'react-fns'
 import FitSVGTextRect from './FitSVGTextRect'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <WindowSize
-          render={({ width, height }) => (
-            <FitSVGTextRect
-              width={width}
-              height={height}
-              text="Always keep smile"
-            />
-          )}
-        />
-      </div>
-    )
-  }
-}
-
+const App = () => (
+  <WindowSize
+    render={({ width, height }) => (
+      <FitSVGTextRect width={width} height={height} text="Always keep smile" />
+    )}
+  />
+)
 export default App
