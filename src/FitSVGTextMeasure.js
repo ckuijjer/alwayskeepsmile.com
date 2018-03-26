e5d5f989477ccf6ae36cc8086e1193ff299e717e
Casper Kuijjer <casper@kuijjer.com>
2018-03-26 21:35:06 +0200

    Removed comments and fixed special characters at end of text

--- src/FitSVGTextMeasure.js
+++ src/FitSVGTextMeasure.js
@@ -4,34 +4,18 @@ import getCompositions from './compositions'
 import FitSVGText from './FitSVGText'
 
 export default class FitSVGTextMeasure extends React.Component {
-  constructor(props) {
-    super(props)
+  textElement = null
 
-    this.state = {
-      zoom: 1,
-      linesOfText: null,
-      compositions: null,
-    }
+  state = {
+    compositions: null,
   }
 
   componentDidMount() {
     this.handleTextChanged()
-    // this.handleRestOfChanges()
   }
-
-  // componentWillReceiveProps(nextProps) {
-  //   // todo: add other properties, and handle text change (don't do a thing on text change?)
-  //   if (
-  //     nextProps.width !== this.props.width ||
-  //     nextProps.height !== this.props.height
-  //   ) {
-  //     this.handleRestOfChanges()
-  //   }
-  // }
 
   componentDidUpdate(prevProps) {
     if (prevProps.text !== this.props.text) {
-      // the text has been changed and rendered
       this.handleTextChanged()
     }
   }
@@ -39,10 +23,8 @@ export default class FitSVGTextMeasure extends React.Component {
   handleTextChanged() {
     const { text } = this.props
 
-    // - get the compositions
-    // - get the bounding box of the text
-    // - get the width of each of the words
-    // - get the height of the text
+    // get the height of the text
+    const height = this.textElement.getBBox().height
 
     // find the indices where words are, e.g. "hi you" => [[0, 1], [3, 5]]
     const indices = this.findWordIndices(text)
@@ -50,67 +32,30 @@ export default class FitSVGTextMeasure extends React.Component {
     // create all possible ways this can be split into lines, e.g. [[[[[0, 1]], [[3, 5]]], [[0, 1], [3, 5]]]
     const compositions = getCompositions(indices)
 
-    // const containerHeight = height
-    // const containerWidth = width
-
-    // based on the currently rendered element (but without the transform: scale)
-    // and including spaces between the words (when you split over multiple lines spaces become less)
-    // although with this.textElement.getSubStringLength it's all good
-    const height = this.textElement.getBBox().height
-
     compositions.forEach(lines => {
       lines.forEach(words => {
         const startOfFirstWord = words[0][0]
         const endOfLastWord = words[words.length - 1][1]
         const numberOfCharacters = endOfLastWord - startOfFirstWord
 
-        // based on the currently rendered element (but without the transform: scale)
         const width = this.textElement.getSubStringLength(
           startOfFirstWord,
           numberOfCharacters,
         )
 
         words.width = width
-        // words.maximumHorizontalZoom = containerWidth / Math.ceil(width)
       })
 
-      // const height = textHeight * lines.length
-
       lines.height = height * lines.length
-      // lines.maximumVerticalZoom = containerHeight / Math.ceil(height)
-      // lines.maximumHorizontalZoom = Math.min(
-      //   ...lines.map(x => x.maximumHorizontalZoom),
-      // )
-      // lines.maximumZoom = Math.min(
-      //   lines.maximumVerticalZoom,
-      //   lines.maximumHorizontalZoom,
-      // )
     })
 
     this.setState({
       compositions,
     })
-
-    // compositions.sort((a, b) => b.maximumZoom - a.maximumZoom)
-    // const best = compositions[0]
-
-    // const nextState = {
-    //   zoom: best.maximumZoom,
-    //   linesOfText: best.map(words => {
-    //     const startOfFirstWord = words[0][0]
-    //     const endOfLastWord = words[words.length - 1][1]
-
-    //     return text.substring(startOfFirstWord, endOfLastWord)
-    //   }),
-    // }
-
-    // this.setState(nextState)
   }
 
-  // handleRestOfChanges() {}
-
   findWordIndices = str => {
-    const regex = /\w+/g
+    const regex = /\S+/g
 
     const result = []
     let match
@@ -162,6 +107,9 @@ export default class FitSVGTextMeasure extends React.Component {
 
     return (
       <Fragment>
+        <text {...restProps} ref={c => (this.textElement = c)} stroke="#f99">
+          {text}
+        </text>
         {linesOfText && (
           <FitSVGText
             {...restProps}
@@ -171,9 +119,6 @@ export default class FitSVGTextMeasure extends React.Component {
             zoom={zoom}
           />
         )}
-        <text {...restProps} ref={c => (this.textElement = c)} stroke="#f99">
-          {text}
-        </text>
       </Fragment>
     )
   }
