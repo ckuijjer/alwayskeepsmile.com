1dd22faaf1d020d572c0ac2795375c0597c4dd0e
Casper Kuijjer <casper@kuijjer.com>
2018-03-26 21:31:50 +0200

    Testing the text prop of FitSVGText

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react'
 import { WindowSize } from 'react-fns'
 import FitSVGTextRect from './FitSVGTextRect'
 
-const App = () => (
-  <WindowSize
-    render={({ width, height }) => (
-      <FitSVGTextRect width={width} height={height} text="One two threeee" />
-    )}
-  />
-)
+class App extends Component {
+  state = {
+    text: 'One two three',
+  }
+
+  onChange = e => {
+    this.setState({ text: e.target.value || '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.text} onChange={this.onChange} />
+        <WindowSize
+          render={({ width, height }) => (
+            <FitSVGTextRect
+              width={width}
+              height={height}
+              text={this.state.text}
+            />
+          )}
+        />
+      </div>
+    )
+  }
+}
+
 export default App
